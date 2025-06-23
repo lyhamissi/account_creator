5261dfaa9287aa7699b5ca99aa4769694a6236e1
Afua Hamissi <lyhamissi@example.com>
2025-06-23 14:39:51 +0200

    created basic files to be used

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link to='/home'></Link>
+      <p>home</p>
+    </div>
+  )
+}
+
+export default Navbar
